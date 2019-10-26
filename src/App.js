4e042dd31fa961f5e020d9a7c39a6ccb6212fc73
Florian <florian.tournay@hotmail.fr>
2019-10-26 03:14:56 +0200

    Add alphabetical filter and pagination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import Dropdown from 'react-dropdown'
 import logo from './assets/images/logo.svg';
 import noPoster from './assets/images/iconmonstr-television-20.svg';
+import ReactPaginate from 'react-paginate';
+
 import './App.css';
+import 'react-dropdown/style.css'
 
 class HomePage extends Component {
   constructor(props){
@@ -13,58 +17,111 @@ class HomePage extends Component {
       page_list: 1,
       old_page_list: null,
       res: null,
-      alpha_filter: true,
-      opened_alpha: false
+      alpha_filter: 'asc',
+      opened_alpha: false,
+      date_filter: null,
+      genre_filter: null
     };
+    this.optionsAlpha = [
+      { value: 'asc', label: 'Ordre Alphabétique' },
+      { value: 'desc', label: 'Ordre Inversé' }
+    ];
+    this.defaultOptionAlpha = this.optionsAlpha[0];
   }
 
   componentDidMount(){
-    let order = this.state.alpha_filter?'asc':'desc';
-    fetch(this.state.base_url+'discover/movie?sort_by=original_title.'+order+'&api_key='+this.state.api_key+'&page=1')
+    fetch(this.state.base_url+'discover/movie?sort_by=original_title.'+this.state.alpha_filter+'&api_key='+this.state.api_key+'&page=1')
     .then(response => response.json())
     .then((data) => {
-      this.setState({res: data, old_page_list: 1});
+      this.setState({res: data});
     });
   }
 
-  componentDidUpdate(){
-    // let order = this.state.alpha_filter?'asc':'desc';
-    // if(this.state.page_list !== this.state.old_page_list){
-    //   fetch(this.state.base_url+'discover/movie?sort_by=original_title.'+order+'&api_key='+this.state.api_key+'&page='+this.state.page_list)
-    //   .then(response => response.json())
-    //   .then((data) => {
-    //     this.setState({res: data, old_page_list: this.state.page_list});
-    //   });
-    // }
+  componentDidUpdate(prevState){
+    // console.log('UPDATE');
+    console.log(prevState.res);
+    console.log(this.state.res);
+    //console.log(JSON.stringify(this.state))
+    if(JSON.stringify(prevState) !== JSON.stringify(this.state)){
+      console.log('UPDATING DATAS');
+
+      // fetch(this.state.base_url+'discover/movie?sort_by=original_title.'+this.state.alpha_filter+'&api_key='+this.state.api_key+'&page='+this.state.page_list)
+      // .then(response => response.json())
+      // .then((data) => {
+      //   this.setState({res: data});
+      // });
+
+    } else {
+      console.log('BOTH ARE THE SAME STATE')
+    }
+  }
+
+  refreshMovieList(alpha){
+    // console.log('refreshMovieList');
+    // console.log(this.state.base_url+'discover/movie?sort_by=original_title.'+alpha+'&api_key='+this.state.api_key+'&page='+this.state.page_list)
+    // fetch(this.state.base_url+'discover/movie?sort_by=original_title.'+alpha+'&api_key='+this.state.api_key+'&page='+this.state.page_list)
+    // .then(response => response.json())
+    // .then((data) => {
+    //   this.setState({res: data, alpha_filter: alpha});
+    // });
   }
 
   renderMoviesList(){
     let movies = this.state.res;
-    console.log(movies);
+    // console.log(movies);
     return (
       movies && movies.results.map((movie, index) =>
         <li key={index} className="w_movie">
           <div className="movie">
             {
-                movie.poster_path ? 
-                <img src={this.state.img_url + movie.poster_path} alt={'Poster'+ movie.title} />
-                :<div className="noPoster"><img src={noPoster} alt={'Poster'+ movie.title} /></div>
+              movie.poster_path ? 
+              <img src={this.state.img_url + movie.poster_path} alt={'Poster'+ movie.title} />
+              :<div className="noPoster"><img src={noPoster} alt={'Poster'+ movie.title} /></div>
             }
             <p className='title'>{movie.title}</p>
-            <p className='released'>{movie.release_date.split('-')[0]}</p>
+            <p className='released'>{movie.release_date ? movie.release_date.split('-')[0] : 'Année inconnue'}</p>
           </div>
         </li>
       )
     )
   }
 
-  handleClickAlpha(e){
-    e.stopPropagation();
-    console.log(this.state.opened_alpha);
-    this.setState({opened_alpha: !this.state.opened_alpha})
+  onChangeAlpha(entry){
+    console.log(entry.value);
+    this.setState({alpha_filter: entry.value})
+    //this.refreshMovieList(entry.value)
   }
 
+  renderPagination(){
+    if(this.state.res){
+      let totalPages = this.state.res.total_pages;
+      let currentPage = this.state.res.page;
+      let maxdisplayedPages = 10;
+      console.log('totalPages : '+totalPages);
+      console.log('currentPage : '+currentPage);
+      console.log('maxdisplayedPages : '+maxdisplayedPages);
 
+      return (
+        <ReactPaginate 
+          initialPage={currentPage} 
+          pageCount={totalPages} 
+          pageRangeDisplayed={maxdisplayedPages} 
+          marginPagesDisplayed='0'
+          previousLabel=' '
+          nextLabel=' '
+          containerClassName='pagination'
+          pageClassName='pagination-btn'
+          nextClassName='pagination-btn nextPage'
+          previousClassName='pagination-btn prevPage'
+          onPageChange={(e) => this.changePage(e)}
+        />
+      )
+    }
+  }
+
+  changePage(e){
+    console.log(e)
+  }
 
   render() {
     return (
@@ -77,20 +134,13 @@ class HomePage extends Component {
         <div className="container">
           <h3>Tous les films</h3>
           <div className="filters">
-          <div>Trier par: 
-            <button className="alphaBlock" onClick={(e) => this.handleClickAlpha(e)} opened={this.state.opened_alpha.toString()}>
-              <span>Ordre Alphabétique</span>
-              <ul>
-                <li order="asc">Ordre Alphabétique</li>
-                <li order="desc">Ordre Inversé</li>
-              </ul>
-            </button>
-          </div>
+            <span>Trier par:</span>
+            <Dropdown options={this.optionsAlpha} onChange={(entry) => this.onChangeAlpha(entry)} value={this.state.alpha_filter} placeholder="Choisir l'ordre d'apparition" className="alphaBlock"/>
           </div>
           <ul className="moviesPage">
             {this.renderMoviesList()}
           </ul>
-
+          {this.renderPagination()}
         </div>
       </div>
     );
